8bf3929879a69eded523861b3741fd6e69869c2f
feel-coding <khe628@naver.com>
2020-01-20 18:19:07 +0900

    created a project

---
+++ app/src/main/java/my/study/planner/DBHelper.java
@@ -0,0 +1,24 @@
+package my.study.planner;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class DBHelper extends SQLiteOpenHelper {
+    public DBHelper(@Nullable Context context) {
+        super(context, "planner.db", null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("create table planners(_id integer primary key autoincrement, todo text not null, date date not null, done integer not null);");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("drop table if exists memos;");
+        onCreate(db);
+    }
+}
---
+++ app/src/main/java/my/study/planner/Planner.java
@@ -0,0 +1,13 @@
+package my.study.planner;
+
+public class Planner {
+    String todo;
+    String date;
+    int done;
+
+    public Planner(String todo, String date, int done) {
+        this.todo = todo;
+        this.date = date;
+        this.done = done;
+    }
+}
