f041ae453f9d64e720598850c204f0fd3a419df7
feel-coding <khe628@naver.com>
2020-01-24 23:41:56 +0900

    백버튼 추가

--- app/src/main/java/my/study/planner/SearchActivity.java
+++ app/src/main/java/my/study/planner/SearchActivity.java
@@ -1,6 +1,8 @@
 package my.study.planner;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 
@@ -10,5 +12,13 @@ public class SearchActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+        Toolbar toolbar = (Toolbar)findViewById(R.id.tb2);
+        setSupportActionBar(toolbar);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayShowCustomEnabled(true); //커스터마이징 하기 위해 필요
+        actionBar.setDisplayShowTitleEnabled(true);
+        actionBar.setDisplayHomeAsUpEnabled(true); // 뒤로가기 버튼, 디폴트로 true만 해도 백버튼이 생김
+        actionBar.setHomeAsUpIndicator(R.drawable.backbutton);
     }
 }
