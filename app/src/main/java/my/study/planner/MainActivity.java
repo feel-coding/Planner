1d0556e776562d76d2aa1f49c87bb1a4a8882fc3
feel-coding <khe628@naver.com>
2020-01-28 03:30:41 +0900

    네비게이션 드로어 열렸을 때 키보드 내리기

--- app/src/main/java/my/study/planner/MainActivity.java
+++ app/src/main/java/my/study/planner/MainActivity.java
@@ -207,8 +207,12 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         });
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         NavigationView navigationView = findViewById(R.id.nav_view);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close){
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                imm.hideSoftInputFromWindow(editText.getWindowToken(), 0);
+            }
+        };
         drawer.addDrawerListener(toggle);
         toggle.syncState();
         navigationView.setNavigationItemSelectedListener(this);
