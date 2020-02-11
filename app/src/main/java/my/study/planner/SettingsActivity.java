2f334b9a9b122bf92be3e78d6bbe558ba795a6f2
feel-coding <khe628@naver.com>
2020-02-11 23:05:37 +0900

    리스너 하나 삭제

--- app/src/main/java/my/study/planner/SettingsActivity.java
+++ app/src/main/java/my/study/planner/SettingsActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.util.Log;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
@@ -30,9 +31,14 @@ public class SettingsActivity extends AppCompatActivity implements Preference.On
             @Override
             public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
                 if(key.equals("password")){
+                    Log.d("pwpw", "익명 객체");
                     if(sharedPreferences.getBoolean("password", false)) {
+                        Log.d("pwpw", "맞으면");
                         Intent i = new Intent(SettingsActivity.this, PasswordSettingActivity.class);
                         startActivity(i);
+                    }
+                    else {
+
                     }
                 }
             }
@@ -43,10 +49,7 @@ public class SettingsActivity extends AppCompatActivity implements Preference.On
 
     @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
-        if(newValue.equals(true)) {
-            Intent i = new Intent(SettingsActivity.this, PasswordSettingActivity.class);
-            startActivity(i);
-        }
+        Log.d("pwpw", "인터페이스의 메소드 구현");
         return true;
     }
     public static class SettingsFragment extends PreferenceFragmentCompat {
